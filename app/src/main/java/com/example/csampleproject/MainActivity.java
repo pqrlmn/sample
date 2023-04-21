9e0faab2824459559b665cd3e2757a47648a6b7c
Pqr <pqrlmn2022@gmail.com>
2023-04-21 11:50:02 +0530

    Commit sample project.

--- app/src/main/java/com/example/csampleproject/MainActivity.java
+++ app/src/main/java/com/example/csampleproject/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("TAG", "This is the sample project for check (test)");
+        Log.d("TAG", "This is the updated sample project for check (test)");
     }
 }
